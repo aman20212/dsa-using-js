38ad2aa29ab79314052f8888069aa770ab69b195
Aman Singh <aman.singh04@nagarro.com>
2024-08-11 07:41:27 +0530

    Multiple Pointers Problems

---
+++ BigO/countUniqueValues.js
@@ -0,0 +1,33 @@
+/*Implement a function called countUniqueValues which accepts a sorted array and counts the uique values in the array.There can be negative numbers in the array, but it will always be sorted */
+function countUniqueValues(arr) {
+    if (arr.length === 0) {
+        return 0;
+    }
+
+    let count = 1;
+    let i = 0;
+
+    for (let j = 1; j < arr.length; j++) {
+        if (arr[j] !== arr[i]) {
+            count++;
+            i = j; // Move i to the new unique value
+        }
+    }
+
+    return count;
+}
+
+/*=================Solution2============================== */
+function countUniqueValues(arr) {
+    if (arr.length == 0) {
+        return 0;
+    }
+    let i = 0;
+    for (let j = 1; j < arr.length; j++) {
+        if (arr[j] !== arr[i]) {
+            i++;
+            arr[i] = arr[j];
+        }
+        return i + 1;
+    }
+}
